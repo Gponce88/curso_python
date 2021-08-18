a1d42511d5a915eaf3a041a048905e49db298422
Gponce88 <89085477+Gponce88@users.noreply.github.com>
2021-08-18 11:40:57 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 18 11:33:33 2021
+
+@author: GabyPonce
+"""
+
+first_name= input('Ingrese su primer nombre: ')
+last_name= input('Ingrese su apellido: ')
+location= input('Ingrese su ubicacion (Quito): ')
+age= input(' Ingrese su edad: ')
+
+space= ' '
+
+print(first_name+space+last_name+space+location+space+age)
